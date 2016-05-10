e84d3d910d52e8b1709ed23900c80c771bf6d01a
sanjeev singh <snjvsingh123@gmail.com>
2016-05-10 17:10:16 -0400

    clean 130 examples with full title

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,9 +4,7 @@ describe "Static Pages" do
   subject { page }
 
   describe "home page" do
-    it "should have the title AFC | Home" do
-      visit  '/static_pages/home'
-      should have_title "AFC | Home"
-    end
+    before { visit '/static_pages/home' }
+    it { should have_title full_title('Home') } 
   end
 end
