fc2b018ab5c83560b84ae47b792eca1c6dc8eb71
sanjeev singh <snjvsingh123@gmail.com>
2016-05-11 13:04:26 -0400

    passing signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  resources :users
+  match '/signup', to: 'users#new', via: 'get'
 
+  get 'static_pages/home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
