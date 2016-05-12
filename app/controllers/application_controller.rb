6e7a982fdfefef39cf25a6a4b00e12b3ac3c280b
sanjeev singh <snjvsingh123@gmail.com>
2016-05-12 16:59:02 -0400

    fixing user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Include all authentication code in controllers. These are already
+  # available in all the views.
+  include SessionsHelper
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,30 @@
 module SessionsHelper
+
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
+
+  # Set method.
+  def current_user=(user)
+    @current_user = user
+  end
+
+  # Get method. For each request we read the db for the user token.
+  # If used more than once in a request then do memonization.
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  # Check if a user is signed in ?
+  def signed_in?
+    !@current_user.nil?
+  end
+
+  # Sign out a user.
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
 end
